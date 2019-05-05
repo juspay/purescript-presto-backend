bdfcdadc998216b255d5fdeb7b03cf17066e41ea
newspecies <harsh21592@gmail.com>
2019-05-06 04:19:29 +0530

    added basic test

--- test/Main.purs
+++ test/Main.purs
@@ -19,17 +19,69 @@
  along with this program. If not, see <https://www.gnu.org/licenses/agpl.html>.
 -}
 
-module Main where
+module Test.Main where
 
 import Prelude
+
+import Cache (CACHE, SimpleConn, CacheConnOpts, newConn, host, port, socketKeepAlive, db)
+import Control.Monad.Aff (Aff, launchAff_)
+import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Exception (throwException)
+import Control.Monad.Except.Trans (runExceptT)
+import Control.Monad.Reader.Trans (runReaderT)
+import Control.Monad.State.Trans (runStateT)
+import Data.Either (Either(..))
+import Data.Options ((:=), Options)
+import Data.StrMap (StrMap, singleton)
+import Presto.Backend.Flow (BackendFlow, log)
+import Presto.Backend.Interpreter (BackendRuntime(..), Connection(..), runBackend)
+import Presto.Core.Types.API (Request(..))
+import Debug.Trace (spy)
 
-apiWrapper :: forall t2. Applicative t2 => t2 Unit
-apiWrapper = do
+type Config = {
+    test :: Boolean
+}
+
+configs = { test : true}
+
+newtype FooState = FooState { test :: Boolean}
+
+fooState = FooState { test : true}
+
+apiRunner :: ∀ e. Request → Aff e String
+apiRunner r = pure "add working api runner!"
+
+redisOptions :: Options CacheConnOpts
+redisOptions = host := "127.0.0.1"
+         <> port := 6379
+         <> db := 0
+         <> socketKeepAlive := true
+
+connections :: Connection -> StrMap Connection
+connections conn = singleton "DB" conn
+
+logRunner :: forall e a. String -> a -> Aff _ Unit
+logRunner tag value = pure (spy tag) *> pure (spy value) *> pure unit
+
+foo :: BackendFlow FooState Config Unit
+foo = log "foo" "ran" *> pure unit
+
+tryRedisConn :: forall e. Options CacheConnOpts -> Aff _ SimpleConn
+tryRedisConn opts = do
+    eCacheConn <- newConn opts
+    case eCacheConn of
+         Right c -> pure c
+         Left err -> liftEff $ throwException err
+
+main :: forall t1. Eff _ Unit
+main = launchAff_ start *> pure unit
+
+start :: forall t1. Aff _ Unit
+start = do
+    conn <- tryRedisConn redisOptions
+    let backendRuntime = BackendRuntime apiRunner (connections (Redis conn)) logRunner
+    response  <- liftAff $ runExceptT ( runStateT ( runReaderT ( runBackend backendRuntime (foo)) configs) fooState)
     pure unit
-
-main :: forall t1. Eff ( console :: CONSOLE | t1) Unit
-main = do
-    log "I am awesome"
